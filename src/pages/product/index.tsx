38fff3ceae1b5e26570c55fde19a367714634b3a
Juan Cahuana <jcahuanam@gmail.com>
2020-06-21 13:55:56 -0500

    Add responsive styles and header in producto page

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -7,21 +7,27 @@ function ProductPage() {
   return (
     <div className="container-xl">
       <div className="ProductPage">
-        <div className="ProductPage__sidebar">
+        <aside className="ProductPage__sidebar">
           <ProductFilter />
-        </div>
-        <div className="ProductPage__content">
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-        </div>
+        </aside>
+        <main className="ProductPage__content">
+          <header className="ProductPage__header d-flex justify-content-between align-items-baseline mb-4">
+            <h1 className="h4 text-primary lh-1 mb-0">Solo planes</h1>
+            <div className="h6 text-primary mb-0">Mostrando <b>06</b> de <b>06</b> productos</div>
+          </header>
+          <section className="ProductPage__productList">
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+          </section>
+        </main>
       </div>
     </div>
   )
