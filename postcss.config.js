1c2c47d762d07d2323d9ea4e14cb0ab79f294b1e
Juan Cahuana <jcahuanam@gmail.com>
2020-06-28 23:27:32 -0500

    Install and configure tailwindcss bases

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+const tailwindcss = require('tailwindcss');
+
+module.exports = {
+  plugins: [
+    // require('postcss-import'),
+    tailwindcss('tailwind.config.js'),
+    require('postcss-nested'),
+    require('autoprefixer')
+  ]
+}
